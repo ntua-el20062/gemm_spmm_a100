76ff61ba3d0ba65ed15280d82eaf8a1e445256f6
Areti Mei (diplom [el20062@mail.ntua.gr]) <amei@epyc7.cslab.ece.ntua.gr>
2026-01-21 17:19:42 +0200

    added time plots on spmm for readmostly hint

--- spmm_managed_explicit/run.sh
+++ spmm_managed_explicit/run.sh
@@ -4,10 +4,9 @@
 #nsys profile --trace=cuda,osrt --cuda-memory-usage=true --cuda-um-gpu-page-faults=true --cuda-um-cpu-page-faults=true -o um_profile_managed_C_mem_advise ./spmm_managed_C_mem_advise ../../suitesparse_mats/ldoor/ldoor.mtx 128 > results_%_managed_C_mem_advise_ldoor.txt;
 #python3 plot_mem_managed_C_mem_advise.py;
 
-#nsys profile --trace=cuda,osrt --cuda-memory-usage=true --cuda-um-gpu-page-faults=true --cuda-um-cpu-page-faults=true -o um_profile_managed_A ./spmm_managed_A ../../suitesparse_mats/ldoor/ldoor.mtx 128 > results_%_managed_A_ldoor.txt;
-#python3 plot_mem_managed_A.py;
+nsys profile --trace=cuda,osrt --cuda-memory-usage=true --cuda-um-gpu-page-faults=true --cuda-um-cpu-page-faults=true -o um_profile_managed_A ./spmm_managed_A suitesparse/ldoor/ldoor.mtx 128 
 
-#nsys profile --trace=cuda,osrt --cuda-memory-usage=true --cuda-um-gpu-page-faults=true --cuda-um-cpu-page-faults=true -o um_profile_managed_C ./spmm_managed_C ../../suitesparse_mats/ldoor/ldoor.mtx 128 > results_%_managed_C_ldoor.txt;
+nsys profile --trace=cuda,osrt --cuda-memory-usage=true --cuda-um-gpu-page-faults=true --cuda-um-cpu-page-faults=true -o um_profile_managed_C ./spmm_managed_C suitesparse/ldoor/ldoor.mtx 128 
 #python3 plot_mem_managed_C.py;
 
 #./spmm_managed_A_mem_advise ../spmm/suitesparse/ldoor/ldoor.mtx 128 > results_%_managed_A_mem_advise_ldoor.txt;
@@ -22,5 +21,5 @@
 #./spmm_managed_C ../spmm/suitesparse/ldoor/ldoor.mtx 128 > results_%_managed_C_ldoor.txt;
 #python3 plot_mem_managed_C.py;
 
-./spmm_explicit_A ../spmm/suitesparse/ldoor/ldoor.mtx 128 > results_%_explicit_A_ldoor.txt;
-python3 plot_mem_explicit_A.py;
+#./spmm_explicit_A ../spmm/suitesparse/ldoor/ldoor.mtx 128 > results_%_explicit_A_ldoor.txt;
+#python3 plot_mem_explicit_A.py;
